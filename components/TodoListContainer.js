2ce11f0c8a40965d7a220d4b9d22b515aa687912
wy <wuyi@yiqixie.com>
2015-05-16 11:41:18 +0800

    many bugs about ListView

--- components/TodoListContainer.js
+++ components/TodoListContainer.js
@@ -1,8 +1,10 @@
 var React = require('react-native');
 var styles = require('./styles');
-var TodoActions = require('../actions/TodoActions');
-var TodoStore = require('../stores/TodoStore');
-var TodoListItem = require('./TodoListItem');
+var Footer = require('./Footer');
+//var Header = require('./Header');
+// var TodoActions = require('../actions/TodoActions');
+// var TodoStore = require('../stores/TodoStore');
+// var TodoListItem = require('./TodoListItem');
 
 
 var {
@@ -13,6 +15,7 @@ var {
   ListView,
   TouchableHighlight,
   AlertIOS,
+  Navigator,
 } = React;
 
 
@@ -20,58 +23,67 @@ var {
 
 var TodoListContainer = React.createClass({
 
-	componentWillMount: function() {
-	    this.dataSource = new ListView.DataSource({
-	      rowHasChanged: (row1, row2) => row1 !== row2
-	    });
-	  },
+  // componentWillMount: function() {
+	 //    this.dataSource = new ListView.DataSource({
+	 //      rowHasChanged: (row1, row2) => row1 !== row2
+	 //    });
+  // },
 
-	_AlertMenu:function(rowData,rowID){
-		//AlertIOS.alert('you opened item No.'+rowID);
-	},
+ //  _AlertMenu:function(rowData,rowID){
+ 
+ //  },
 
-	_openItem:function(rowData,rowID){
-		console.log(rowData);
-	},
+ //  _openItem:function(rowData,rowID){
+ //  	console.log("lets go to open the item");
+ //    console.log(rowData);
+ //    // this.props.navigator.push({
+ //    //   title:rowData && rowData/text || 'New Item',
+ //    //   component:ToDoEdit,
+ //    //   passProps:{item:rowData,id:rowID,update:this._updateItem}
+ //    //});
+
+	
+ //  },
+
+ //  _createItem:function(){
+ //    TodoActions.createItem();
 
 
-	_renderRow:function(rowData:string,sectionID:number,rowID:number){
-		return(
-			<TodoListItem 
-			 item = {rowData}
-			 onPress = {this._openItem(rowData,rowID)}
-			 onLongPress = {this._AlertMenu(rowData,rowID)} />
+ //  },
+
+
+ //  _updateItem: function(item, index){
+ //      // var items = this.props.todos;
+ //      // if (index) {
+ //      //   items[index] = item;
+ //      // }
+ //      // else {
+ //      //   items.push(item);
+ //      // }
+ //      // this.setState({items: items});
+ //      console.log('now you want to close the pop window');
+
+ //      //this.props.navigator.pop();
+ //  },
+
+	// _renderRow:function(rowData:string,sectionID:number,rowID:number){
+	// 	return(
+	// 		<TodoListItem 
+	// 		 item = {rowData} />
+	// 		 // onPress = {this._openItem(rowData,rowID)}
+	// 		 // onLongPress = {this._updateItem(rowData,rowID)} />
 			
-			);
-	},
-
-	_createItem:function(){
-		TodoActions.createItem();
-
-
-
-
-	},
+	// 	);
+	// },
 
 	render:function(){
-		var dataSource = this.dataSource.cloneWithRows(this.props.todos)
-
+		//var dataSource = this.dataSource.cloneWithRows(this.props.todos);
+		var length = this.props.todos;
 		return(
-		   <View>
-			 	<ListView 
-			 		style ={styles.listContainer}
-			 		dataSource ={dataSource}
-			 		renderRow ={this._renderRow}/>
-
-			 	<TouchableHighlight
-	          style={styles.button}
-	          onPress={this._createItem}>
-	            <Text style={styles.buttonText}>+</Text>
-	            </TouchableHighlight>
-	       </View>
+		       <Footer/>
 			);
 
-	}
+	},
 
 
 
---
+++ components/code.js
@@ -0,0 +1,9 @@
+			 	// <ListView 
+			 	// 	style ={styles.listContainer}
+			 	// 	dataSource ={dataSource}
+			 	// 	renderRow ={this._renderRow}/>
+			 	// <TouchableHighlight
+			  //        style={styles.button}
+			  //        onPress={this._openItem}>
+			  //        <Text style={styles.buttonText}>+</Text>
+	    //         </TouchableHighlight>
