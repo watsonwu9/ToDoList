2c7406669014bce492e299cc2be9eb107d1dc140
wy <wuyi@yiqixie.com>
2015-05-16 11:41:37 +0800

    Revert a417a41..2ce11f0
    This rolls back to commit a417a41631f208a87551a1a984f263a3f67d9938.

--- components/code.js
+++
@@ -1,9 +0,0 @@
-			 	// <ListView 
-			 	// 	style ={styles.listContainer}
-			 	// 	dataSource ={dataSource}
-			 	// 	renderRow ={this._renderRow}/>
-			 	// <TouchableHighlight
-			  //        style={styles.button}
-			  //        onPress={this._openItem}>
-			  //        <Text style={styles.buttonText}>+</Text>
-	    //         </TouchableHighlight>
--- components/index.ios.js
+++ components/index.ios.js
@@ -6,25 +6,64 @@
 
 var React = require('react-native');
 var styles = require('./styles');
-var MainView = require('./MainView');
 
-
+var Footer =require('./Footer');
+var TodoListContainer = require('./TodoListContainer');
+var Header = require('./Header');
+var TodoStore = require('../stores/TodoStore');
 
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Navigator,
-  NavigatorIOS,
 } = React;
 
+function _getTodoState() {
+  return {
+    todos:TodoStore.getAll()
+  };
+};
+
+
 var ToDoList = React.createClass({
+
+  componentDidMount:function(){
+    TodoStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount:function(){
+    TodoStore.removeChangeListener(this._onChange);
+  },
+
+  getInitialState:function(){
+    return _getTodoState();
+  },
+
   render: function() {
+    // if (Object.keys(this.state.allTodos).length < 1) {
+    //   return null;
+    // };
+
+
+    var length = this.state.todos.length;
+
+
     return (
-     <MainView />
+      <View style={styles.container}>
+        <Header/>
+        <TodoListContainer
+          todos={this.state.todos}/>
+        <Footer/>
+        <Text>there are {length} todos</Text>
+      </View>
     );
   },
+  _onChange:function(){
+    console.log(this.state.todos);
+    console.log('_onChange called ');
+    this.setState(_getTodoState());
+    console.log(this.state.todos);
+  }
 });
 
 
